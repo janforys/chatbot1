e7dcc8b7146748bb676fc3a6b69db04d14d0244e
janforys <janforys@wp.pl>
2023-04-14 01:06:42 +0200

    chatbot

---
+++ chatbot1/src/chatbot1/chatbotMain.java
@@ -0,0 +1,23 @@
+package chatbot1;
+
+import java.util.Scanner;
+
+public class chatbotMain {
+
+	public static void main(String[] args) {
+		int levelOfHapinness = 10;
+		
+		String request = "How can I help you?";
+		System.out.println(request);
+		
+		Scanner in = new Scanner(System.in);
+		String userInput = in.nextLine();
+		
+		//System.out.println(userInput);
+		
+		System.out.println("Did you said " + userInput + "?");
+		userInput = in.nextLine();
+		System.out.println("That's great");
+		
+	}	
+}
