cc65b47a09a4b2b4aa736c5ea26f906c7a6c6663
janforys <janforys@wp.pl>
2023-04-18 20:47:37 +0200

    loop

--- chatbot1/src/chatbot1/chatbotMain.java
+++ chatbot1/src/chatbot1/chatbotMain.java
@@ -7,22 +7,29 @@ public class chatbotMain {
 	public static void main(String[] args) {
 		
 		int levelOfHapinness = 10;
-		
 		String request = "How can I help you?";
-		System.out.println(request);
+		String defaultResponse = "That's great!";
+		String happySign = "<3";
 		
+		while(true) {	
+			
+		System.out.println(request);
 		Scanner in = new Scanner(System.in);
 		String userInput = in.nextLine();
 		
 		if (userInput.length() > 3) {
-			System.out.println("I am glad to talk with you.");
-		} else {
-			System.out.println("Did you said " + userInput + "?");
+			levelOfHapinness++;
 		}
 		
-		userInput = in.nextLine();
+		String response = defaultResponse;
 		
-		System.out.printf(String.format("That's great, I am happy is %d and yours?%n", levelOfHapinness));
-		System.out.println("?");
+		if (levelOfHapinness > 11) {
+			response = response + ' ' + happySign;
+		}
+		
+		System.out.println(response);
+		
+		}
+		
 	}	
 }
