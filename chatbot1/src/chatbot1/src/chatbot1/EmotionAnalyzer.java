c45027e0a8420dc0e69df3643d580c086c9908b2
janforys <janforys@wp.pl>
2023-04-30 13:59:39 +0200

    compatibility version check

--- chatbot1/src/chatbot1/src/chatbot1/EmotionAnalyzer.java
+++ chatbot1/src/chatbot1/src/chatbot1/EmotionAnalyzer.java
@@ -3,19 +3,19 @@ package chatbot1.src.chatbot1;
 import java.util.List;
 
 class EmotionAnalyzer {
-	
-	int levelOfHappiness = 10; 
-	
-	public void analyzeInput(String userInput) throws Exception {
-		
-		List<String> words = List.of(userInput.split(" "));
-		
-		if (words.size() == 0) {
-			throw new Exception("Empty input");
-		}
-		
-		if (words.size() > 1) {
-			levelOfHappiness++;
-		}
-	}
+
+    int levelOfHappiness = 10;
+
+    void analyzeInput(String userInput) throws Exception {
+        List<String> words = List.of(userInput.split(" "));
+
+        if (words.isEmpty()) {
+            throw new Exception("There is no words");
+        }
+
+        if (words.size() > 1) {
+            levelOfHappiness++;
+        }
+    }
+
 }
--- chatbot1/src/chatbot1/src/chatbot1/chatbotMain.java
+++ chatbot1/src/chatbot1/src/chatbot1/chatbotMain.java
@@ -3,40 +3,42 @@ package chatbot1.src.chatbot1;
 import java.util.Scanner;
 
 public class chatbotMain {
-	
-	private static String getResponse(int levelOfHappiness, int iteration) {
-		
-		String[] responses = { "Thats great", "You're awesome", "Nice to hear it" };
-		String defaultResponse = responses[iteration % responses.length];
-		String happySign = ",,|,,";
-		String response = defaultResponse;
-		
-		if (levelOfHappiness > 11) {
-			response += ' ' + happySign;
-		}
-		return response;
-	}
-	
-	public static void main(String[] args) {
-		
-		EmotionAnalyzer emotionAnalyzer = new EmotionAnalyzer();
-		Scanner in = new Scanner(System.in);
-		int iteration = 0;
-		String request = "How can I help you?";
-		System.out.println(request);
-		
-		while (true) {		
-			String userInput = in.nextLine();
-			String response;
-			
-			try {
-				emotionAnalyzer.analyzeInput(userInput);
-				response = chatbotMain.getResponse(emotionAnalyzer.levelOfHappiness, iteration);
-			} catch (Exception e) {
-				response = "What did you said ?";
-			}
-			System.out.println(response);
-			iteration++;
-		}	
-	}	
+
+    private static String getResponse(int levelOfHappiness, int iteration) {
+        String[] responses = {"That's great", "You are awesome!",
+                "Nice to hear it"};
+
+        String defaultResponse = responses[iteration % responses.length];
+        String happySign = ",,|,,";
+        String response = defaultResponse;
+
+        if (levelOfHappiness > 11) {
+            response += ' ' + happySign;
+        }
+
+        return response;
+    }
+
+    public static void main(String[] args) {
+        EmotionAnalyzer emotionAnalyzer = new EmotionAnalyzer();
+        Scanner in = new Scanner(System.in);
+        int iteration = 0;
+        String request = "How can I help you?";
+        System.out.println(request);
+
+        while (true) {
+            String userInput = in.nextLine();
+            String response;
+
+            try {
+                emotionAnalyzer.analyzeInput(userInput);
+                response = getResponse(emotionAnalyzer.levelOfHappiness, iteration);
+            } catch (Exception e) {
+                response = "What did you said?";
+            }
+
+            System.out.println(response);
+            iteration++;
+        }
+    }
 }
